298a0c907cb3125f72dfda850e071adf3cddb12d
mummuk <zenodzzang@gmail.com>
2026-05-28 15:24:10 +0900

    modify main

--- calc/calc/calc.cpp
+++ calc/calc/calc.cpp
@@ -1,9 +1,15 @@
 ﻿// calc.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+
 
 #include <iostream>
+#include "add.h"
+#include "mul.h"
+#include "sub.h"
+
 
 int main()
 {
-    
+    printf("1 + 2 = %d\n", add(1, 2));
+    printf("1 - 2 = %d\n", sub(1, 2));
+    printf("1 * 2 = %d\n", mul(1, 2));
 }
